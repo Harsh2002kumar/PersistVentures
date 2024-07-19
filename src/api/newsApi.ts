d4f549c07862b3881ea9b7214744b385499da0a1
Harsh2002kumar <harshkumarpandey2002@gmail.com>
2024-07-19 14:28:46 +0530

    first commit

---
+++ src/api/newsApi.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_NEWS_API_KEY
+  ? import.meta.env.VITE_NEWS_API_KEY
+  : // : "c8b3145d5c274d519e48b5eac56dd04e";
+    "b1980e9c63914fc6bf219d11bc90259a";
+const BASE_URL = "https://newsapi.org/v2";
+
+export const fetchNewsFromApi = (category: string, page: number) => {
+  const url = `${BASE_URL}/top-headlines?country=us&category=${category}&page=${page}&apiKey=${API_KEY}`;
+  return axios.get(url);
+};
